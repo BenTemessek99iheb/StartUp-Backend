ded7d1e6d8c32d89b04680f456040fdcdbf750fd
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2024-01-20 20:25:59 +0100

    Mapper Adj

--- src/main/java/com/example/startup/mapper/EstablishmentMapper.java
+++ src/main/java/com/example/startup/mapper/EstablishmentMapper.java
@@ -2,12 +2,11 @@ package com.example.startup.mapper;
 
 import com.example.startup.dto.EstablishmentDTO;
 import com.example.startup.entities.Establishment;
+import com.example.startup.util.tools.GenericMapper;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 
-public interface EstablishmentMapper {
-    EstablishmentDTO toDto(Establishment establishment);
+public interface EstablishmentMapper extends GenericMapper<Establishment, EstablishmentDTO>  {
 
-    Establishment toEntity(EstablishmentDTO establishmentDTO);
 }
