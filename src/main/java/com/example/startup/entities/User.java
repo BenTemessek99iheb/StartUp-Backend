84156f2cb5440f472bed731ca349f0499f08270e
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2024-01-20 02:39:15 +0100

    Client Table

--- src/main/java/com/example/startup/entities/User.java
+++ src/main/java/com/example/startup/entities/User.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "Client")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User implements Serializable {
 
