584f2e1e81235f52e9d14c019084b296d84b4e08
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2024-01-20 16:28:08 +0100

    Controllers

---
+++ src/main/java/com/example/startup/controllers/EstablishmentController.java
@@ -0,0 +1,7 @@
+package com.example.startup.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EstablishmentController {
+}
---
+++ src/main/java/com/example/startup/controllers/EventController.java
@@ -0,0 +1,4 @@
+package com.example.startup.controllers;
+
+public class EventController {
+}
