c729af48807d337c5dcc479ae36de1946930de0f
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2024-01-24 01:21:07 +0100

    ReservationEstablishment

--- src/main/java/com/example/startup/services/IReservationEstablishmentService.java
+++ src/main/java/com/example/startup/services/IReservationEstablishmentService.java
@@ -1,5 +1,24 @@
 package com.example.startup.services;
 
+import com.example.startup.dto.ReservationEstablishmentDTO;
+import com.example.startup.dto.ReservationEventDTO;
+import com.example.startup.entities.ReservationEstablishment;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface IReservationEstablishmentService {
+    public ReservationEstablishmentDTO createReservationEstablishment(UUID userId, UUID establishmentId, ReservationEstablishmentDTO reservationEstablishmentDTO);
+
+    public ReservationEstablishmentDTO getReservationEstablishment(UUID id);
+
+    public List<ReservationEstablishmentDTO> getAllReservationEstablishments();
+
+    public ReservationEstablishmentDTO updateReservationEstablishment(UUID id, ReservationEstablishmentDTO reservationEstablishmentDTO);
+
+    public void deleteReservationEstablishment(UUID id);
 
 }
+
+
+
