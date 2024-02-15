0b5bcbb53138f9b78d0e6944c5d668c0b0176a98
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2024-02-15 16:55:35 +0100

    Adding Bean @Repo

--- src/main/java/com/example/startup/repositories/CategoryRepo.java
+++ src/main/java/com/example/startup/repositories/CategoryRepo.java
@@ -2,8 +2,9 @@ package com.example.startup.repositories;
 
 import com.example.startup.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface CategoryRepo extends JpaRepository<Category, UUID> {
 }
